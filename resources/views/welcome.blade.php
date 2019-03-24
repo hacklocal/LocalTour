29cf7815c2b27fa2a58a60ebb92af4e869594e27
Simone Lanini <lanini.simo@gmail.com>
2019-03-24 14:13:59 +0100

    Final

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,167 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE html>
+<!--[if IE 7]> <html class="no-js ie7 oldie" lang="en-US"> <![endif]-->
+<!--[if IE 8]> <html class="no-js ie8 oldie" lang="en-US"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html lang="zxx">
     <head>
+        <!-- TITLE OF SITE -->
+        <title> {{ setting('site.title') }} </title>
+        
+        <!-- Meta -->
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="app landing page template" />
+        <meta name="keywords" content="app, landing page, bootstrap" />
+        <meta name="developer" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-        <title>Laravel</title>
+        <!-- FAV AND ICONS   -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+        <link rel="shortcut icon" href="assets/images/apple-icon.png">
+        <link rel="shortcut icon" sizes="72x72" href="assets/images/apple-icon-72x72.png">
+        <link rel="shortcut icon" sizes="114x114" href="assets/images/apple-icon-114x114.png">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <!-- GOOGLE FONTS -->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700%7cRRaleway:400,500" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+        <!-- FONT ICONS -->
+        <link rel="stylesheet" href="assets/icons/font-awesome-4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/font/flaticon.css">
+    
+        <!-- Bootstrap-->
+        <link rel="stylesheet" href="assets/plugins/css/bootstrap.min.css">
+        <!-- Animation -->
+        <link rel="stylesheet" href="assets/plugins/css/animate.css">
+        <!-- owl -->
+        <link rel="stylesheet" href="assets/plugins/css/owl.css">
+        <!--flexslider-->
+        <link rel="stylesheet" href="assets/plugins/css/flexslider.min.css">
+        <!-- selectize -->
+        <link rel="stylesheet" href="assets/plugins/css/selectize.css">
+        <link rel="stylesheet" href="assets/plugins/css//selectize.bootstrap3.css">
+        <!-- Fancybox-->
+        <link rel="stylesheet" href="assets/plugins/css/jquery.fancybox.min.css">
+        <link rel="stylesheet" href="assets/plugins/css/bootstrap-dropdownhover.min.css">
+        <link rel="stylesheet" href="assets/css/simple-line-icons.css">
 
-            .full-height {
-                height: 100vh;
-            }
+        <!-- COUSTOM CSS link  -->
+        <link rel="stylesheet" href="assets/less/style.css">
+        <link rel="stylesheet" href="assets/less/responsive.css">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        <!-- Colors -->
+        <link rel="stylesheet" href="assets/less/colors/blue.css" title="blue">
+        <link rel="alternate stylesheet" href="assets/less/colors/green.css" title="green">
+        <link rel="alternate stylesheet" href="assets/less/colors/orange.css" title="orange">
+        <link rel="alternate stylesheet" href="assets/less/colors/purple.css" title="purple">
+        <link rel="alternate stylesheet" href="assets/less/colors/slate.css" title="slate">
+        <link rel="alternate stylesheet" href="assets/less/colors/yellow.css" title="yellow">
+        <link rel="alternate stylesheet" href="assets/less/colors/red.css" title="red">
+        <link rel="alternate stylesheet" href="assets/less/colors/blue-munsell.css" title="blue-munsell">
 
-            .position-ref {
-                position: relative;
-            }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        <!--[if lt IE 9]>
+            <script src="js/plagin-js/html5shiv.js"></script>
+            <script src="js/plagin-js/respond.min.js"></script>
+        <![endif]-->
 
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+    <body class="ldx-black">
+        <header>
+            <!-- 
+            |========================
+            |  Nav
+            |========================
+            -->
+            <div class="main-navigation">
+                <nav class="navbar navbar-fixed-top nav-scroll" id="navbar">
+                    <div class="container">
+                        <div class="row">
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-action="#js-navbar-menu" aria-expanded="false">
+                                        <span aria-hidden="true" class="icon"></span>
+                                    </button>
+                                <!-- page brand -->
+                                <a class="navbar-brand" href="index.html"><img src="assets/images/logo.png" alt="" class="img-responsive"></a>
+                            </div>
+                            <div class="collapse navbar-collapse black-nav" id="js-navbar-menu">
+                                <ul class="nav navbar-nav navbar-right" id="navbar-nav">
+                                    <li><a href="http://localhost:8000/admin/login">Accedi</a></li>
+                                    <li><a href="http://localhost:8000/admin/login">Registrati</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </nav><!-- /.navbar -->
+            </div>
+            <!-- 
+            |========================
+            |   HOME CONTENT
+            |========================
+            -->
+            <div class="fixed-bg image-bg ldx-home-black-bg ldx-home" id="ldx-home">
+                <div class="ldx-home-overlay gradeint-bg">
+                    <div class="container">
+                        <div class="row section-separator">
+                            <div class="col-sm-4">
+                                <img src="assets/images/dx-iphone.png" alt="" class="img-responsive wow fadeInRight" data-wow-duration="0.8s" data-wow-delay="0.s">
+                            </div>
+                            <div class="col-sm-8">
+                                <div class="ldx-home-content">
+                                    <h1 class="wow fadeInUp" data-wow-duration="0.6s" data-wow-delay="0.2s">Scopri le bellezze nascoste del tuo quartiere </h1>
+                                    <p class="wow fadeInUp" data-wow-duration="0.6s" data-wow-delay="0.5s">Scarica ora l'app e scopri cosa ti sei perso! </p>
+                                    <div class="button">
+                                        <a href="" class="wow fadeInUp" data-wow-duration="0.6s" data-wow-delay="0.7s">
+                                             <img src="assets/images/appstore.png" alt="" class="img-responsive">
+                                        </a>
+                                        <a href="" class="wow fadeInUp" data-wow-duration="0.6s" data-wow-delay="0.7s">
+                                             <img src="assets/images/playstore.png" alt="" class="img-responsive">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </header>
+        <!--
+        |========================
+        |      Script
+        |========================
+        -->
+        
+        <!-- jquery -->
+        <script src="assets/plugins/js/jquery-1.11.3.min.js"></script>
+        <!-- Bootstrap -->
+        <script src="assets/plugins/js/bootstrap.min.js"></script>
+        <!-- mean menu nav-->
+        <!--<script src="assets/plugins/js/meanmenu.js"></script>-->
+        <!-- ajaxchimp -->
+        <script src="assets/plugins/js/jquery.ajaxchimp.min.js"></script>
+        <!-- wow -->
+        <script src="assets/plugins/js/wow.min.js"></script>
+        <!-- Owl carousel-->
+        <script src="assets/plugins/js/owl.carousel.js"></script>
+        <!--flexslider-->
+        <script src="assets/plugins/js/jquery.flexslider-min.js"></script>
+        <!--dropdownhover-->
+        <script src="assets/plugins/js/bootstrap-dropdownhover.min.js"></script>
+        <!--jquery-ui.min-->
+        <script src="assets/plugins/js/bars.js"></script>
+        <!--validator -->
+        <script src="assets/plugins/js/validator.min.js"></script>
+        <!--smooth scroll-->
+        <script src="assets/plugins/js/smooth-scroll.js"></script>
+        <!-- Fancybox js-->
+        <script src="assets/plugins/js/jquery.fancybox.min.js"></script>
+        <script src="assets/plugins/js/jquery.fitvids.js"></script>
+        <script src="assets/plugins/js/jquery.nav.js"></script>
+        <!-- SELECTIZE-->
+        <script src="assets/plugins/js/standalone/selectize.js"></script>
+        <!-- isotope js-->
+        <script src="assets/plugins/js/isotope.pkgd.js"></script>
+        <script src="assets/plugins/js/packery-mode.pkgd.js"></script>
+        <!-- init -->
+        <script src="assets/js/init.js"></script>
+        
     </body>
 </html>
