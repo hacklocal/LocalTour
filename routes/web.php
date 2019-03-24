804fbe0a6f506354518734ab063a6468eeb64d83
Simone Lanini <lanini.simo@gmail.com>
2019-03-24 05:39:21 +0100

    Build 1.00.00

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,22 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
+
+//Route::apiResource('point/{id}', 'API\PointController');
+
+Route::get('point/{id}', 'API\PointController@show');
+
+Route::post('newpoint', 'API\PointController@store');
+
+/*
+Route::get('/test', function () {
+    return view('test');
+});
+*/
+
+Route::redirect('/666', 'https://www.officetally.com/wp-content/uploads/2008/09/dwight-ascii.pdf', 302);
